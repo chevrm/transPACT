53e4e77b1c9adc869e4f96cf1b1f6e3457fe2a27
Marc Chevrette <chevrm@gmail.com>
2020-03-24 15:47:36 -0500

    yet again

--- data/dendrogram20200320/cladetsv2tbl.pl
+++ data/dendrogram20200320/cladetsv2tbl.pl
@@ -17,6 +17,7 @@ while(<$cfh>){
     $ks =~ s/-$//;
     $ks = 'KS5' if($ks eq 'KS55'); # 9-methylstreptimidone_KS55
     my $clust = join('|', @q[0..$#q-1]);
+    $ks = 'KS17' if($ks eq 'KS' && $clust eq 'phormidolide');
     if($query =~ m/OUTGROUP/){
 	$ks = 'KS1';
 	if($query =~ m/2qo3/){
@@ -25,7 +26,9 @@ while(<$cfh>){
 	    $clust = 'OUTGROUP_2hg4|chainA_EryKS5';
 	}
     }
-    $kscount{$clust} += 1;
+    if($clade =~ m/^Clade/){
+	$kscount{$clust} += 1;
+    }
     $clustks{$clust}{$ks} = $clade;
     $n += 1;
 }
@@ -40,30 +43,27 @@ foreach my $n (1..4){
     push @order, 'KS'.$n.'b';
 }
 print "\t".join("\t", @order)."\n";
-foreach my $clust (sort keys %clustks){
+foreach my $clust (sort keys %kscount){
+    next unless($kscount{$clust} > 1 || $clust =~ m/^OUTGROUP/);
     my $ksstring = '';
-    my $ct = 0;
+    my $tot = scalar(keys %{$clustks{$clust}});
     foreach my $ks (@order){
 	if(exists $clustks{$clust}{$ks}){
-	    $ct += 1;
 	    if($clustks{$clust}{$ks} =~ m/^Clade/ || $clustks{$clust}{$ks} eq 'clade_not_conserved'){
 		if($ksstring eq ''){
 		    $ksstring = $clustks{$clust}{$ks};
 		}else{
 		    $ksstring .= "\t".$clustks{$clust}{$ks};
 		}
-	    }else{
-		last;
-	    }	
+	    }#else{
+		#last;
+	    #}	
 	}
     }
-    foreach my $n ($ct..scalar(@order)-1){
+    foreach my $n ($tot..scalar(@order)-1){
 	$ksstring .= "\t".'NA';
     }
-    die "You dont know how to count\n" unless(scalar(@order) == scalar(split(/\t/, $ksstring)));
-    if($ksstring ne '' && $ct > 1){
-	print "$clust\t$ksstring\n";
-    }elsif($clust =~ m/^OUTGROUP/){
-	print "$clust\t$ksstring\n";
-    }
+    my ($o, $g) = (scalar(@order), scalar(split(/\t/, $ksstring)));
+    die "You dont know how to count\n$clust\n$ksstring\n$o\t$g\n" unless($o == $g);
+    print "$clust\t$ksstring\n";
 }
